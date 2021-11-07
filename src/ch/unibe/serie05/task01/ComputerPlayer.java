7cbc07eba64246ffde867e3791bf27d522a1f4ff
fabian <fabian@scaly.ch>
2021-11-07 10:45:03 +0100

    implemented more intelligent computer player in Vier Gewinnt task.

--- src/ch/unibe/serie05/task01/ComputerPlayer.java
+++ src/ch/unibe/serie05/task01/ComputerPlayer.java
@@ -12,18 +12,81 @@ public class ComputerPlayer implements IPlayer
 	/**
 	 * Strategy is to chose a random column and select 
 	 * the next valid column to the right (the chosen included)
+	 *
+	 * Extended (more intelligent, but not enough intelligent at all):
+	 * We perform the following operations in the given order and priority:
+	 *  - check if there is a col with which the opponent will finish the game. If yes, then choose this one.
+	 *  - check if there is a col with which the computer can finish the game. If yes, choose this col.
+	 *  - else we choose a random col which isn't already full
 	 */
 	public int getNextColumn( Token[][] board )
 	{
-		java.util.Random generator = new java.util.Random();
-		int col = generator.nextInt( board.length );
-		int step = 0;
-		while ( isColFull( col, board ) ) {
-			col = ( col + 1 ) % board.length;
+		Token opponentsToken = (this.token == Token.player1)? Token.player2: Token.player1;
+		int opponentColToFinish = checkNextColumn(board, opponentsToken);
+		int myColToFinish = checkNextColumn(board, this.token);
+
+		if(opponentColToFinish != -1) {
+			return opponentColToFinish;
+		} else if(myColToFinish != -1) {
+			return myColToFinish;
+		} else {
+			java.util.Random generator = new java.util.Random();
+			int col = generator.nextInt( board.length );
+			int step = 0;
+			while ( isColFull( col, board ) ) {
+				col = ( col + 1 ) % board.length;
+			}
+			return col;
 		}
-		return col;
 	}
 
+
+	/**
+	 * checks if there is any field with which we can finish the game.
+	 * Not covered cases:
+	 * 	- detect only the first option. if there are several, they won't get detected.
+	 * 	- if there are right and left of the field few tokens, then it won't get detected by this method.
+	 *
+	 * @param board board to check
+	 * @param tokenToCheck the token to check with
+	 * @return if a column is found it will return the column number. Otherwise, it will return -1.
+	 */
+	private int checkNextColumn(Token[][] board, Token tokenToCheck) {
+		// check variable for up, down, left and right
+		int colToChoose = -1;
+
+		// Loop over all fields. if the field is empty, check if we can finish the game, if we put there a token.
+		for (int col = 0; col < board.length; col++) {
+			for (int row = 0; row < board[col].length; row++) {
+				if(board[col][row] == Token.empty) {
+					// check variable for down, left and right
+					boolean fourInColDown = true, fourInRowRight = true, fourInRowLeft = true;
+
+					// check variable downwards diagonal directions
+					boolean diagonalDownLeft = true, diagonalDownRight = true;
+
+					for (int i = 1; i < 4; i++) {
+						// check down, right and left (-/+ are switched because origin is down left)
+						fourInRowRight = ((col + i) < board.length && fourInRowRight && tokenToCheck == board[col + i][row] );
+						fourInRowLeft = ((col - i) >= 0 && fourInRowLeft && tokenToCheck == board[col - i][row] );
+						fourInColDown = ((row - i) >= 0  && fourInColDown && tokenToCheck == board[col][row - i]);
+
+						// checks both downwards diagonal directions (-/+ are switched because origin is down left)
+						diagonalDownLeft = ((col + i) < board.length && (row - i) >= 0 && diagonalDownLeft
+								&& tokenToCheck == board[col + i][row - i]);
+						diagonalDownRight = ((col - i) >= 0 && (row - i) >= 0 && diagonalDownRight
+								&& tokenToCheck == board[col - i][row - i] );
+					}
+
+					// if the checked field can finish the game in one direction, choose this row and finish check
+					if (fourInColDown || fourInRowLeft || fourInRowRight
+							|| diagonalDownLeft || diagonalDownRight)
+						return col;
+				}
+			}
+		}
+		return colToChoose;
+	}
 
 
 	/**
